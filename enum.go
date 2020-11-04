5f3eed550761d197483a36b3a113878f3cb3edc9
pzm <pzm@servyou.local>
2020-11-04 15:19:08 +0800

    init

--- enum.go
+++
@@ -1,5 +0,0 @@
-package functions
-
-const (
-	RpcxBasePath = "/rpcx"
-)
--- rpcx.go
+++ rpcx.go
@@ -7,26 +7,31 @@ import (
 	"github.com/smallnest/rpcx/server"
 	"github.com/smallnest/rpcx/serverplugin"
 	"log"
+	"net"
 	"time"
 )
 
-type RpcxAddrInfo struct {
+type RpcxInfo struct {
+	AppName string
 	ServiceAddr string
 	ConsulAddr string
 	RpcxBasePath string
 }
 
 const (
+	FlagAppName = "app.name"
 	FlagRpcxPort = "rpcx.port"
 	FlagRpcxConsulAddr = "rpcx.consul.addr"
 	FlagRpcxBasePath = "rpcx.base.path"
 )
 
-func GetRpcxInfo() RpcxAddrInfo {
+func GetRpcxInfo() RpcxInfo {
+	appName := agollo.Get(FlagAppName)
 	serviceAddr := fmt.Sprintf("%s:%s", GetHostIP(), agollo.Get(FlagRpcxPort))
 	consulAddr := agollo.Get(FlagRpcxConsulAddr)
 	basePath := agollo.Get(FlagRpcxBasePath)
-	return RpcxAddrInfo{
+	return RpcxInfo{
+		AppName: appName,
 		ServiceAddr: serviceAddr,
 		ConsulAddr:  consulAddr,
 		RpcxBasePath: basePath,
@@ -48,4 +53,19 @@ func AddConsulRegistryPlugin(s *server.Server, basePath, addr, consulAddr string
 	}
 
 	s.Plugins.Add(r)
+	s.Plugins.Add(&ConnectionPlugin{})
+}
+
+type ConnectionPlugin struct {
+}
+
+
+func (p *ConnectionPlugin) HandleConnAccept(conn net.Conn) (net.Conn, bool) {
+	log.Printf("client %v connected", conn.RemoteAddr().String())
+	return conn, true
+}
+
+func (p *ConnectionPlugin) HandleConnClose(conn net.Conn) bool {
+	log.Printf("client %v closed", conn.RemoteAddr().String())
+	return true
 }
